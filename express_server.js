19a36309d168464bc855c6119ecf36a784fd04da
Dong Hu <cdhu@uwaterloo.ca>
2017-05-10 19:38:28 +0000

    updating urls

--- express_server.js
+++ express_server.js
@@ -1,20 +1,22 @@
-// import modules
+// Dependencies
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
-// envrionment setup
+// Envrionment setup & configuration
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
 
-// global variables
+// Global variables
 const PORT = process.env.PORT || 8080; // default port 8080
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-// generate a random shortURL
+// Middleware
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Generate a random shortURL
 function generateRandomString() {
     let newURL = "";
     const dictionary = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -26,7 +28,7 @@ function generateRandomString() {
 
 const newUserId = generateRandomString()
 
-// request responses
+// Routes
 app.get("/", (req, res) => {
   res.end("Hello!");
 });
@@ -45,7 +47,7 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id, urls: urlDatabase};
+  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
 });
 
@@ -55,6 +57,18 @@ app.get("/u/:shortURL", (req,res) => {
 
 app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+// server-side - handles the post request to delete URL
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect("/urls");
+});
+
+// server-side - handles the post request to update URL
+app.post("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id] = req.body.newURL;
+  res.redirect("/urls");
 });
 
 app.post("/urls", (req, res) => {
