a90c05f7ea28fe80016b01c686e7a9e0678ddf5c
Dong Hu <cdhu@uwaterloo.ca>
2017-05-10 00:31:00 +0000

    redirect after form submission

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 // import modules
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 // envrionment setup
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
 // global variables
 const PORT = process.env.PORT || 8080; // default port 8080
@@ -12,6 +14,19 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+// generate a random shortURL
+function generateRandomString() {
+    let newURL = "";
+    const dictionary = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i = 0; i < 6; i++) {
+      newURL += dictionary[Math.floor(Math.random() * dictionary.length)];
+    }
+   return newURL;
+ }
+
+const newUserId = generateRandomString()
+
+// request responses
 app.get("/", (req, res) => {
   res.end("Hello!");
 });
@@ -20,18 +35,32 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
-});
-
 app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id, urls: urlDatabase};
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req,res) => {
+  res.redirect(urlDatabase[req.params.shortURL]);
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.post("/urls", (req, res) => {
+  let newShortURL = generateRandomString();
+  urlDatabase[newShortURL] = req.body.longURL;
+  res.redirect(`/urls/${newShortURL}`);
 });
 
 app.listen(PORT, () => {
